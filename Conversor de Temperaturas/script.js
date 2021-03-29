42868a7380a2bdd050557c1dd3ce6f5352c8aa9b
paulovictor1997 <paulovictornunes97@gmail.com>
2021-03-29 19:55:59 -0300

    Updates

--- Conversor de Temperaturas/script.js
+++ Conversor de Temperaturas/script.js
@@ -9,35 +9,35 @@ function confirmar(){
         temperatura = 'De Celsius para Fahrenheint'
         if(temperatura == 'De Celsius para Fahrenheint'){
            num = 1.8 * num + 32
-           resultado.innerHTML = `${temperatura} a conversão é : ${num} Fº`
+           resultado.innerHTML = `${temperatura} a conversão de ${numero.value}Cº é : ${num} Fº`
         }
         }
         if(temp[1].checked){
             temperatura = 'De Fahrenheint para Celsius'
             if(temperatura =='De Fahrenheint para Celsius'){
                num = (num - 32) * 5/9
-               resultado.innerHTML = `${temperatura} a conversão é : ${num} Cº`
+               resultado.innerHTML = `${temperatura} a conversão de ${numero.value}Fº é : ${num} Cº`
             }
         }
         if(temp[2].checked){
             temperatura = 'De Kelvin para Celsius'
             if(temperatura == 'De Kelvin para Celsius'){
                 num = num - 273
-                resultado.innerHTML = `${temperatura} a conversão é : ${num} Cº`
+                resultado.innerHTML = `${temperatura} a conversão de ${numero.value}Kº é : ${num} Cº`
             }
         }
         if(temp[3].checked){
             temperatura = 'De Celsius para Kelvin'
             if(temperatura ==  'De Celsius para Kelvin' ){
                 num = num + 273
-                resultado.innerHTML = `${temperatura}a conversão fica : ${num} Kº` 
+                resultado.innerHTML = `${temperatura}a conversão de ${numero.value}Cº fica : ${num} Kº` 
             }
         }
         if(temp[4].checked){
             temperatura = 'De Fahrenheint para Kelvin'
             if(temperatura == 'De Fahrenheint para Kelvin'){
                 num = (num -32) * 5/9 + 273
-                resultado.innerHTML = `${temperatura}a conversão fica : ${num} Kº`
+                resultado.innerHTML = `${temperatura}a conversão de ${numero.value}Fº fica : ${num} Kº`
                 
             }
         }
@@ -45,7 +45,7 @@ function confirmar(){
             temperatura = 'De Kelvin para Fahrenheint'
             if(temperatura == 'De Kelvin para Fahrenheint'){
                 num = (num - 273) * 9/5 + 32
-                resultado.innerHTML = `${temperatura} a conversão fica :${num} Fº`
+                resultado.innerHTML = `${temperatura} a conversão de ${numero.value}Kº fica :${num} Fº`
             }
         }
    
