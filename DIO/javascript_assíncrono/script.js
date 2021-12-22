ca3faaf54a3d4f2aa4c07867a08152d2f08c26ee
paulovictor1997 <paulovictornunes97@gmail.com>
2021-12-21 21:48:03 -0300

    Update

--- DIO/javascript_assíncrono/script.js
+++ DIO/javascript_assíncrono/script.js
@@ -1,5 +1,22 @@
-const url = 'https://thatcopy.pw/catapi/rest';
+const url = 'https://thatcopy.pw/catapi/rest/';
 const catBtn = document.getElementById('change-cat');
 const Img = document.getElementById('cat');
 
 const getData = async () =>
+const getCats = async () =>{
+    try{
+        const data = await fetch(url);
+        const json = await data.json();
+        return json.webpurl;
+    } catch(e){
+       console.log(e.message); 
+    }
+}
+
+const loadImg = async ()=>{
+    catImg.src = await getCats();
+}
+
+catBtn.addEventListener('click',loadImg);
+
+loadImg();
