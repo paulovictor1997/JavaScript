aff65bb25577cbc76bc52166a468b12d5d39f73b
paulovictor1997 <paulovictornunes97@gmail.com>
2022-01-10 22:00:10 -0300

    exericio

---
+++ atividades/exe22/assents/script/script.js
@@ -0,0 +1,10 @@
+new Glider(document.querySelector('.glider'), {
+    slidesToShow: 1,
+    draggable: true,
+    dots:'.dots',
+    arrows: {
+      prev: '.glider-prev',
+      next: '.glider-next'
+    },
+  });
+
