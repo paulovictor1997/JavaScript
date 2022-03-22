2eca3e967dbd95445e4282b17c81dc2a35695ae4
paulovictor1997 <paulovictornunes97@gmail.com>
2022-03-21 22:57:21 -0300

    exercicio
    arquivos do exe-27

---
+++ atividades/exe27/script.js
@@ -0,0 +1,19 @@
+function openMenu(){
+    const openButton = document.querySelector('.open-button');
+    const closeButton= document.querySelector('.close-button');
+    const menu = document.querySelector('.menu');
+
+    openButton.classList.add('hidden');
+    closeButton.classList.remove('hidden');
+    menu.classList.add('show');
+}
+
+function closeMenu(){
+    const openButton = document.querySelector('.open-button');
+    const closeButton = document.querySelector('.close-button');
+    const menu = document.querySelector('.menu');
+
+    openButton.classList.remove('hidden');
+    closeButton.classList.add('hidden');
+    menu.classList.remove('show');
+}
