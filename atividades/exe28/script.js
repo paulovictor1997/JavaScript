dcfcbc4ef40d66ab32b736d09d091ce4bfb791e6
paulovictor1997 <paulovictornunes97@gmail.com>
2022-04-15 00:25:53 -0300

    exe 28

--- atividades/exe28/script.js
+++ atividades/exe28/script.js
@@ -12,8 +12,6 @@ form.addEventListener('submit',e =>{
     }
 });
 
-
-
 function validateEmail(email) {
     let regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return regex.test(String(email).toLowerCase());
