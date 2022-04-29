4a1f274657ef7dade3b7694ac2440b0e6efa510f
paulovictor1997 <paulovictornunes97@gmail.com>
2022-04-28 23:36:52 -0300

    exe29

--- atividades/exe29/script.js
+++ atividades/exe29/script.js
@@ -1,9 +1,32 @@
 async function getApi(){
-    const response = await fetch('https://restcountries.com/v3.1/all');
+    const response = await fetch('https://restcountries.com/v3.1/all')
     const data = await response.json();
     if(response.status === 200){
         console.log(data);
-    }
+        showInfo(data);
+    }   
+}
+
+const showInfo = (content) =>{
+    const myData = content.map(({
+        flags,
+        name,
+        capital,
+        continents,
+        population
+    }) =>{
+        return `
+          <div>
+          <img src ="${flags.png}">
+          <p> ${name.common}</p>
+          <p> ${capital}</p>
+          <p> ${continents}</p>
+          <p> ${population}</p>
+          </div>  
+        `
+    })
+    document.querySelector('.teste').innerHTML = myData;
+
 }
 
 getApi()
