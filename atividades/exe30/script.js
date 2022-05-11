83702dcd6967ad70e57cee275678aeb8b4e8f326
paulovictor1997 <paulovictornunes97@gmail.com>
2022-05-10 21:45:41 -0300

    files

---
+++ atividades/exe30/script.js
@@ -0,0 +1,32 @@
+const html = document.querySelector('html');
+const checkbox = document.querySelector('input[name=theme');
+
+const getStyle = (element,style)=>{
+    window.getComputedStyle(element)
+    .getPropertyValue(style)
+}
+
+//Pegando o estilo
+
+const initialColors = {
+    
+}    
+    
+const darkMode = {
+   
+}
+    
+const transformKey = key =>
+    "--" + key.replace(/([A-Z])/, "-$1").toLowerCase()
+
+//Mudar a função padrão
+
+const changeColors = (colors) =>{
+    Object.keys(colors).map(key =>
+        html.style.setProperty(transformKey(key),colors[key])
+    )
+}
+
+checkbox.addEventListener('change',({target})=>{
+    target.checked ? changeColors(darkMode) : changeColors(initialColors)
+})
