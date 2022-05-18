566b4bfd0a497a7389d39cccae5d5026bf7cea6b
paulovictor1997 <paulovictornunes97@gmail.com>
2022-05-17 21:22:10 -0300

    files exe31

---
+++ atividades/exe31/JS/cavs.js
@@ -0,0 +1,26 @@
+const gold = [
+    {id:1, name :'Mu',img:'img/Mu.jpg', idade: 20, born: 'Tibete' ,constellation : 'Áries' , description : 'Treinou em Jamiel, seus principais poderes são : Muralha de Cristal, Revolução estelar, Extinção estelar, Telecinese e a Rede de Cristal'},
+
+    {id:2, name:'Aldebaran', img:'img/Aldebaran.jpg', born :'Brasil', idade: 20, constellation:'Touro', description:'Treinou no Brasil, seus principais poderes são : Grande chifre, Punho de aço, Chifres Cosmicos'},
+
+    {id:3, name:'Saga', img:'img/Saga.jpg', born: 'Grécia', idade: 28, constellation : 'Gêmeos', description:'Treinou no Santuário, seus principais poderes são : Explosão Galáctica, Outra Dimensão,  Satã Imperial, Aniquilação Sensorial '},
+
+    {id:4, name:'Máscara da Morte', img:'img/MDM.jpg', born: 'Itália', idade: 23, constellation : 'Câncer' ,description: 'Treinou na Sicília,seus principais poderes são : Ondas do Inferno, Mergulho dos Espíritos,Separação de Almas'},
+
+    {id:5, name:'Aiolia', img:'img/Aiolia.jpg', born: 'Grécia', idade: 20, constellation : 'Leão',description: 'Treinou no Santuário, seus principais poderes são : Relâmpago de Plasma, Cápsula do Poder, Presas Relâmpago, Invocação de Fótons, Aceleração de Fótons, Explosão de Fótons'},
+
+    {id:6, name:'Shaka', img:'img/Shaka.jpg', born: 'Índia', idade: 20, constellation : 'Virgem',description: 'Treinou na  Índia, seus principais poderes são : Ohm, Tesouro do Céu, Círculo das Seis Existências, Rendição Divina, Invocação dos Espíritos Malígnos '},
+
+    {id:7, name:'Dohko', img:'img/Dohko.jpg', born: 'China', idade: 261, constellation : 'Libra',description: 'Treinou na China nos Cinco Picos de Rozan,seus principais poderes são : Cólera dos cem Dragões, Dragão Voador, Cólera do Dragão, Último Dragão '},
+
+    {id:8, name:'Milo', img:'img/Milo.jpg', born: 'Grécia', idade: 20, constellation : 'Escorpião',description: 'Treinou na ilha de Milos,seus principais poderes são : Agulha Escarlate, Antares,Tempestade Escarlate'},
+
+    {id:9, name:'Aiolos', img:'img/Aiolos.jpg', born: 'Grécia', idade: 14, constellation : 'Sargitário',description: 'Treinou no Santuário,seus principais poderes são : Trovão Atômico, Flecha da Justiça, Destruição Infinita, Impulso de Luz de Quíron.Tinha 14 anos quando morreu'},
+
+    {id:10, name:'Shura', img:'img/Shura.jpg', born: 'Espanha', idade: 23, constellation : 'Capricórnio',description: 'Treinou nos Montes Pirineus,seus principais poderes são : Excalibur, Pedras Saltitantes'},
+
+    {id:11, name:'Camus', img:'img/Camus.jpg', born: 'França', idade: 23, constellation : 'Aquário',description: 'Treinou na Sibéria,seus principais poderes são : Pó de Diamante,Golpe da Constelação de Aquário, Trovão Aurora Ataque, Execução Aurora '},
+
+    {id:12, name:'Afrodite', img:'img/Afrodite.jpg', born: 'Suécia', idade: 22, constellation : 'Peixes',description: 'Treinou na Groenlândia,seus principais poderes são : Rosas Diabólicas Reais, Rosas Piranhas, Rosa Sangrenta, Encomenda de Rosas'},
+]
+
