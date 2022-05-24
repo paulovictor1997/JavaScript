94652882ec73fb5e5ab20de4d54cff797440a03c
paulovictor1997 <paulovictornunes97@gmail.com>
2022-05-23 21:42:28 -0300

    exe 31

--- atividades/exe31/JS/script.js
+++ atividades/exe31/JS/script.js
@@ -1,4 +1,4 @@
-cavaleiros.map((item,index)=>{
+cavaleiros.map((item)=>{
     //Clone Node - Vai fazer uma cópia de um elemento HTML, se dentro dos parênteses estiver "false", ele vai pegar somente o principal, caso contrario se estiver "true", ele vai pegar o elemento que foi especificado.
     let gold = document.querySelector('.models .cavs-item').cloneNode(true);
     document.querySelector('.area').append(gold);
@@ -17,7 +17,7 @@ cavaleiros.map((item,index)=>{
         //Preenchendo os dados
         document.querySelector('.cavsBig img').src = item.img;
         document.querySelector('.name').innerHTML = `${item.name} de ${item.constellation}`;
-        document.querySelector('.born').innerHTML = `País aonde nasceu - ${item.born}`;
+        document.querySelector('.born').innerHTML = `Local de nascimento - ${item.born}`;
         document.querySelector('.age').innerHTML = `Idade - ${item.idade}`;
         document.querySelector('.description').innerHTML = item.description;
     })
@@ -31,6 +31,5 @@ cavaleiros.map((item,index)=>{
     document.querySelector('.mobileButton').addEventListener('click',()=>{
         document.querySelector('.modal-area').style.display = 'none';
     })
-
 
 })
