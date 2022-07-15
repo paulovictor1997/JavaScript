85a2361d3200f69d2d48168789a28c6535be4557
paulovictor1997 <paulovictornunes97@gmail.com>
2022-07-15 00:26:30 -0300

    exe-33

---
+++ atividades/exe33/script.js
@@ -0,0 +1,32 @@
+const html = document.querySelector('html');
+const checkbox = document.querySelector('input[name=theme]');
+
+const getStyle = (element,style)=> {
+    window.getComputedStyle(element)
+    .getPropertyValue(style);
+}
+
+//Pegando o estilo
+
+const initialColors = {
+    bg:getStyle(html,'--bg'),
+    bgcolor:getStyle(html,'--bg-dark')
+}
+
+const darkMode = {
+    bg:'#1e202a',
+    bgcolor:'#ffffff'
+}
+
+const transformKey = key => 
+    "--" + key.replace(/([A-Z])/, "-$1").toLowerCase();
+
+const changeColors = (colors) =>{
+    Object.keys(colors).map(key => 
+        html.style.setProperty(transformKey(key),colors[key]) 
+    )
+}
+
+checkbox.addEventListener('change',({target})=>{
+    target.checked ? changeColors(darkMode) : changeColors(initialColors);
+})
