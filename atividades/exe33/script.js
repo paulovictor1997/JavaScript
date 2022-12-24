34a769bab9bbb07b4993563b2eb2ee47e7bea3de
paulovictor1997 <paulovictornunes97@gmail.com>
2022-12-23 21:28:22 -0300

    update exe33

--- atividades/exe33/script.js
+++ atividades/exe33/script.js
@@ -22,7 +22,7 @@ function showInfo(content){
       <div class= "content-countrie">
       <img src ="${flags.png}">
       <div class = "description">
-      <p> Country - ${name.common}</p>
+      <h4> Country - ${name.common}</h4>
       <p> Capital - ${capital}</p>
       <p> Continent - ${region}</p>
       <p> Region - ${subregion}</p>
@@ -34,22 +34,19 @@ function showInfo(content){
     document.querySelector('#content-area').innerHTML = myArea;
 }
 
-//Pegando as regiões
+//Pegando as regiões e os valores no select
 async function getApiByRegion(region){
   const response = await fetch(`https://restcountries.com/v3.1/region/${region}`)
   const data = await response.json();
   if(response.status === 200){
-     console.log(data); 
       showInfo(data);
   }   
 }
 
-// Pegando os valores no select 
 async function updateSelect(){
   const response = await fetch('https://restcountries.com/v3.1/all')
   const data = await response.json();
     if(response.status === 200){
-      console.log(data); 
       showInfo(data);
     }
 
@@ -67,7 +64,6 @@ async function getApiByname(name){
   const response = await fetch(`https://restcountries.com/v3.1/name/${name}`)
   const data = await response.json();
   if(response.status === 200){
-    console.log(data); 
      showInfo(data);
  }   
 }
