29cceb0972bf39e1d2f31523f7635826a0d92eda
Paulo Victor <81495904+paulovictor1997@users.noreply.github.com>
2023-06-06 10:19:54 -0300

    atividade

--- atividades/exe41/script.js
+++ atividades/exe41/script.js
@@ -2,22 +2,46 @@
     {
       "category": "Reaction",
       "score": 80,
-      "icon": "./assets/images/icon-reaction.svg"
+      "icon": "images/icon-reaction.svg"
     },
     {
       "category": "Memory",
       "score": 92,
-      "icon": "./assets/images/icon-memory.svg"
+      "icon": "images/icon-memory.svg"
     },
     {
       "category": "Verbal",
       "score": 61,
-      "icon": "./assets/images/icon-verbal.svg"
+      "icon": "images/icon-verbal.svg"
     },
     {
       "category": "Visual",
       "score": 72,
-      "icon": "./assets/images/icon-visual.svg"
+      "icon": "images/icon-visual.svg"
     }
   ]
-  
+
+  const showInfo = (content) =>{
+    const myData = content.map(({
+      category,
+      score,
+      icon
+    }) =>{
+        return `
+       <div class="statistic">
+        <div class="icon">
+          <img src="${icon}" alt="icon-svg">
+          <h3>${category}</h3>
+        </div>
+        <div class="info-3">
+          ${score} <span>/ 100</span>
+        </div>
+      </div>
+      
+        `
+    })
+
+    document.querySelector("#content").innerHTML = myData
+}
+
+showInfo(object)
