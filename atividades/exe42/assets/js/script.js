2046167afa48226ecf1128892a081c139ad7c75a
paulovictor1997 <paulovictornunes97@gmail.com>
2023-06-22 21:29:05 -0300

    atividade

---
+++ atividades/exe42/assets/js/script.js
@@ -0,0 +1,11 @@
+function openMenu(){
+    document.querySelector('.open').style.display = 'none';
+    document.querySelector('.close').style.display = 'block';
+    document.querySelector('.menu-mobile').style.display = 'block';
+}
+
+function closeMenu(){
+    document.querySelector('.open').style.display = 'block';
+    document.querySelector('.close').style.display = 'none';
+    document.querySelector('.menu-mobile').style.display = 'none';
+}
