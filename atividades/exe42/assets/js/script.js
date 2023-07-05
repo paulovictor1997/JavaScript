3e97e6ad11b5cf42c119bf3d7ac51a2eca742f38
paulovictor1997 <paulovictornunes97@gmail.com>
2023-07-04 21:29:59 -0300

    atividade

--- atividades/exe42/assets/js/script.js
+++ atividades/exe42/assets/js/script.js
@@ -9,3 +9,25 @@ function closeMenu(){
     document.querySelector('.close').style.display = 'none';
     document.querySelector('.menu-mobile').style.display = 'none';
 }
+
+const simple = document.querySelector('.simple');
+const speedy = document.querySelector('.speedy');
+const easy = document.querySelector('.easy');
+
+simple.addEventListener('click',()=>{
+    document.querySelector('.book-mark-1').style.display='block';
+    document.querySelector('.book-mark-2').style.display='none';
+    document.querySelector('.book-mark-3').style.display='none';
+})
+
+speedy.addEventListener('click',()=>{
+    document.querySelector('.book-mark-2').style.display='block';
+    document.querySelector('.book-mark-1').style.display='none';
+    document.querySelector('.book-mark-3').style.display='none';
+})
+
+easy.addEventListener('click',()=>{
+    document.querySelector('.book-mark-3').style.display='block';
+    document.querySelector('.book-mark-1').style.display='none';
+    document.querySelector('.book-mark-2').style.display='none';
+})
