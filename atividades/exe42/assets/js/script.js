ba2fe44d3d09112a37b9b13fd31ab115464afb13
paulovictor1997 <paulovictornunes97@gmail.com>
2023-07-17 22:00:42 -0300

    atividade

--- atividades/exe42/assets/js/script.js
+++ atividades/exe42/assets/js/script.js
@@ -49,3 +49,27 @@ function toggleAccordion(){
 items.forEach(item => item.addEventListener('click',toggleAccordion));
 
 /* Email Validation*/
+const form = document.querySelector('#form');
+const emailInput = document.querySelector('#email');
+
+form.addEventListener('submit',(event)=>{
+    event.preventDefault();
+    if(emailInput.value === '' || !validateEmail(emailInput.value)){
+        document.querySelector('.error').style.display = 'block';
+       // document.querySelector('.erroIcon').style.display = 'block';
+    } else{
+        document.querySelector('.error').style.display = 'none';
+        document.querySelector('.erroIcon').style.display = 'none';
+    }
+});
+
+function validateEmail(email){
+    const regex = new RegExp(
+        /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]{2,}$/
+    );
+    if(regex.test(email)){
+        return true;
+    } else{
+        return false;
+    }
+}
