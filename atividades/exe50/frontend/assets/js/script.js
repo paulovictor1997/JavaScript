f0c35e3387f7c5c0b7d2c87f28bfde41fc7fc584
Paulo Victor <81495904+paulovictor1997@users.noreply.github.com>
2025-06-27 17:49:31 -0300

    exe 50 - change files

---
+++ atividades/exe50/frontend/assets/js/script.js
@@ -0,0 +1,22 @@
+const getApi = async () => {
+  try {
+    const response = await fetch("https://api.football-data.org/v4/competitions", {
+      method: 'GET',
+      headers: {
+        "X-Auth-Token": "9ec91597457b491393553a765c180372"
+      }
+    })
+    const data = await response.json()
+
+    if (response === 200) {
+      console.log(data)// Mostra todas as competições
+    } else {
+      console.error(`Erro ${response.status}: ${response.statusText}`)
+    }
+
+  } catch (error) {
+    console.error("Erro ao buscar dados:", error)
+  }
+}
+
+getApi()
