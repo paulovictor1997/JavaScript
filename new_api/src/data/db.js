e78ad591155e01d7be414882015b0d289c72dff7
paulovictor1997 <paulovictornunes97@gmail.com>
2026-05-18 16:00:43 -0300

    Update db.js

--- new_api/src/data/db.js
+++ new_api/src/data/db.js
@@ -1,6 +1,7 @@
 let usuarios = [
     {id:1, nome: "Paulo", email:"paulo@email.com"},
     {id:2, nome: "Victor", email:"victor@email.com"},
+    {id:3, nome: "Feitosa", email:"feitosa@email.com"}
 ]
 
 module.exports = usuarios
