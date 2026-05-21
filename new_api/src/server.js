82c31081f2856a70c905429e2e38d47b86ed4258
paulovictor1997 <paulovictornunes97@gmail.com>
2026-05-21 12:22:45 -0300

    update API

--- new_api/src/server.js
+++ new_api/src/server.js
@@ -1,8 +1,10 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const usuariosRoutes = require('./routes/users')
 
 app.use(express.json())
+app.use(cors())
 app.use('/usuarios', usuariosRoutes)
 
 app.listen(3000, ()=> console.log('API rodando em http://localhost:3000'))
