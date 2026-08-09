b6e91e34948b497ef05c6bdcd4cca3d3b28dc870
Paulo Victor <81495904+paulovictor1997@users.noreply.github.com>
2026-08-08 22:14:00 -0300

    update
    Testando o Claude

--- Conversor de Temperaturas/script.js
+++ Conversor de Temperaturas/script.js
@@ -1,54 +1,40 @@
-function confirmar(){
-    let numero = document.querySelector('input#num')
-    let resultado = document.querySelector('div#resultado')
-    let temp = document.getElementsByName('temp') 
-    
-    let num = Number(numero.value)
-    temperatura = ''
-    if(temp[0].checked){
-        temperatura = 'De Celsius para Fahrenheint'
-        if(temperatura == 'De Celsius para Fahrenheint'){
-           num = 1.8 * num + 32
-           resultado.innerHTML = `${temperatura} a conversão de ${numero.value}Cº é : ${num} Fº`
-        }
-        }
-        if(temp[1].checked){
-            temperatura = 'De Fahrenheint para Celsius'
-            if(temperatura =='De Fahrenheint para Celsius'){
-               num = (num - 32) * 5/9
-               resultado.innerHTML = `${temperatura} a conversão de ${numero.value}Fº é : ${num} Cº`
-            }
-        }
-        if(temp[2].checked){
-            temperatura = 'De Kelvin para Celsius'
-            if(temperatura == 'De Kelvin para Celsius'){
-                num = num - 273
-                resultado.innerHTML = `${temperatura} a conversão de ${numero.value}Kº é : ${num} Cº`
-            }
-        }
-        if(temp[3].checked){
-            temperatura = 'De Celsius para Kelvin'
-            if(temperatura ==  'De Celsius para Kelvin' ){
-                num = num + 273
-                resultado.innerHTML = `${temperatura}a conversão de ${numero.value}Cº fica : ${num} Kº` 
-            }
-        }
-        if(temp[4].checked){
-            temperatura = 'De Fahrenheint para Kelvin'
-            if(temperatura == 'De Fahrenheint para Kelvin'){
-                num = (num -32) * 5/9 + 273
-                resultado.innerHTML = `${temperatura}a conversão de ${numero.value}Fº fica : ${num} Kº`
-                
-            }
-        }
-        if(temp[5].checked){
-            temperatura = 'De Kelvin para Fahrenheint'
-            if(temperatura == 'De Kelvin para Fahrenheint'){
-                num = (num - 273) * 9/5 + 32
-                resultado.innerHTML = `${temperatura} a conversão de ${numero.value}Kº fica :${num} Fº`
-            }
-        }
-   
-    numero.value = ''
-    numero.focus()
+function confirmar(event) {
+    if (event) event.preventDefault();
+
+    const numero = document.querySelector('input#num');
+    const resultado = document.querySelector('div#resultado');
+    const radios = document.getElementsByName('temp');
+
+    const num = Number(numero.value);
+    const numFormatado = num.toFixed(2);
+    let temperatura = '';
+
+    if (radios[0].checked) {
+        temperatura = 'De Celsius para Fahrenheint';
+        const convertido = (1.8 * num + 32).toFixed(2);
+        resultado.innerHTML = `${temperatura} a conversão de ${numFormatado}Cº é : ${convertido} Fº`;
+    } else if (radios[1].checked) {
+        temperatura = 'De Fahrenheint para Celsius';
+        const convertido = ((num - 32) * 5 / 9).toFixed(2);
+        resultado.innerHTML = `${temperatura} a conversão de ${numFormatado}Fº é : ${convertido} Cº`;
+    } else if (radios[2].checked) {
+        temperatura = 'De Kelvin para Celsius';
+        const convertido = (num - 273).toFixed(2);
+        resultado.innerHTML = `${temperatura} a conversão de ${numFormatado}Kº é : ${convertido} Cº`;
+    } else if (radios[3].checked) {
+        temperatura = 'De Celsius para Kelvin';
+        const convertido = (num + 273).toFixed(2);
+        resultado.innerHTML = `${temperatura} a conversão de ${numFormatado}Cº fica : ${convertido} Kº`;
+    } else if (radios[4].checked) {
+        temperatura = 'De Fahrenheint para Kelvin';
+        const convertido = ((num - 32) * 5 / 9 + 273).toFixed(2);
+        resultado.innerHTML = `${temperatura} a conversão de ${numFormatado}Fº fica : ${convertido} Kº`;
+    } else if (radios[5].checked) {
+        temperatura = 'De Kelvin para Fahrenheint';
+        const convertido = ((num - 273) * 9 / 5 + 32).toFixed(2);
+        resultado.innerHTML = `${temperatura} a conversão de ${numFormatado}Kº fica : ${convertido} Fº`;
+    }
+
+    numero.value = '';
+    numero.focus();
 }
